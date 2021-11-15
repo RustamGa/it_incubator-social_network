f3a912002f7b3881c849f7111950e3e1d00271e3
RustamGa <gadzhievrustam56@gmail.com>
2021-11-16 00:32:38 +0300

    Lesson_21

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import s from "./Dialogs.module.css"
 export const Dialogs = () => {
     return (
-        <div> Dialogs</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={`${s.dialogs} ${s.active}`}>
+                    Amir
+                </div>
+                <div className ={s.dialog}>
+                    Rustam
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you</div>
+                <div className={s.message}>I'm fine</div>
+            </div>
+        </div>
     )
 }
