e355080319ce467c9407db632b73e935b2b97312
RustamGa <gadzhievrustam56@gmail.com>
2021-11-16 16:18:32 +0300

    Lesson_21

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import s from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom"
+
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
                 <div className={`${s.dialogs} ${s.active}`}>
-                    Amir
+                   <NavLink to = "/dialogs/1">Amir</NavLink>
                 </div>
                 <div className ={s.dialog}>
-                    Rustam
+                    <NavLink to = "/dialogs/2"> Rustam </NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sasha
+                    <NavLink to="/dialogs/3">Sasha</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
