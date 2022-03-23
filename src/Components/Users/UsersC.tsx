782e24ef27c6116ab79ba25c8e690de5d2d32034
RustamGa <gadzhievrustam56@gmail.com>
2022-03-23 22:25:48 +0300

    Lesson_54_life_cycle_methods,componentDidMount

--- src/Components/Users/UsersC.tsx
+++ src/Components/Users/UsersC.tsx
@@ -8,22 +8,25 @@ import axios from "axios";
 // В React.Component зашито поведение что умеют делать все компоненты, если не написать extends React.Component
 // наш объект созданный с помощью класса user не будет обладать нужными умениями чтоб реакт мог с ним взаимодействовать
 export class Users extends React.Component<UsersPropsType> {
-    constructor(props:UsersPropsType) { // так как объект конструируется только один раз и потом возвращается jsx,
-        // в случае когда что то происходит он вызывает только render, а constructor вызывается только один раз и сохраняется
-        // в памяти react, и поэтому мы можем с помощью конструктора сделать запрос на сервер для получения users
-        super(props);
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items)
-            })
-
+    // constructor(props:UsersPropsType) {
+    //     super(props);
+    //         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+    //             this.props.setUsers(response.data.items)
+    //         })
+    // }
+    // удаляем конструктор, так как если он ничего не делает кроме как конструирование свое перебрасывает классу super
+    // то его можно не писать, он вызывается по умолчанию
+    componentDidMount() {// метод жизнего цикла компоненты которая вызывается только олин раз когда перересуется
+        // компонента и передаем ей axios запрос
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
-    getUsers = () => {
 
-    }
 // каждый класс должен определить метод render потому что react будет требовать от этого объекта получить jsx разметку
-render() {
-    return <div>
-        {this.props.usersPage.users.map((u) => <div key={u.id}>
+    render() {
+        return <div>
+            {this.props.usersPage.users.map((u) => <div key={u.id}>
             <span>
                 <div>
                     <img src={UserPhoto} className={styles.usersPhoto}/>
@@ -33,7 +36,7 @@ render() {
                         <button onClick={() => this.props.unFollow(u.id)}>follow</button>}
                 </div>
             </span>
-            <span>
+                <span>
     <span>
         <div>{u.name}</div>
         <div>{u.status}</div>
@@ -43,8 +46,8 @@ render() {
         <div>{'u.location.city'}</div>
     </span>
 </span>
-        </div>)
-        }
-    </div>;
-}
+            </div>)
+            }
+        </div>;
+    }
 }
